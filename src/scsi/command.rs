529c6b93c12fe307b78028b8140b0b7678f46585
arc <zleyyij@users.noreply.github.com>
2026-01-31 14:32:55 -0700

    refactor/feat(scsi): create x10 command descriptor and migrate to the CommandDescriptor trait

--- src/scsi/command.rs
+++ src/scsi/command.rs
@@ -46,7 +46,7 @@ impl CommandBlock<'_> {
 /// Defined in SPC2 7.25
 pub fn test_unit_ready() -> CommandBlock<'static> {
     CommandBlock {
-        command: ShortCommandDescriptor {
+        command: X6CommandDescriptor {
             operation_code: OpCode::TestUnitReady,
             logical_block_address: [0, 0, 0],
             misc_len: 0,
@@ -65,7 +65,7 @@ pub fn test_unit_ready() -> CommandBlock<'static> {
 /// Defined in SPC2 7.3.1 table 45
 pub fn inquiry() -> CommandBlock<'static> {
     CommandBlock {
-        command: ShortCommandDescriptor {
+        command: X6CommandDescriptor {
             operation_code: OpCode::Inquiry,
             logical_block_address: [0, 0, 0],
             // For inquiry, is ALLOCATION LENGTH,
@@ -88,7 +88,7 @@ pub fn inquiry() -> CommandBlock<'static> {
 /// SPC-2 7.12
 pub fn prevent_allow_medium_removal() -> CommandBlock<'static> {
     CommandBlock {
-        command: ShortCommandDescriptor {
+        command: X6CommandDescriptor {
             operation_code: OpCode::PreventAllowMediumRemoval,
             logical_block_address: [0, 0, 0],
             // See table 78, prohibits all form of medium removal
@@ -117,9 +117,9 @@ pub enum OpCode {
     PreventAllowMediumRemoval = 0x13,
 }
 
-/// As described in 4.3.2 table 1, a typical CDB for 6 byte commands.
+/// As described in SPC-2 4.3.2 table 1, a typical CDB for 6 byte commands.
 #[repr(C, packed)]
-struct ShortCommandDescriptor {
+struct X6CommandDescriptor {
     ///"The `OPERATION CODE` field contains the code value identifying the operation
     /// being requested by the CDB. SAM-2 defines the general structure of the operation
     /// code value. The `OPERATION CODE` field has a consistently defined meaning across
@@ -149,19 +149,42 @@ struct ShortCommandDescriptor {
     pub control: u8,
 }
 
-impl ShortCommandDescriptor {
-    // TODO: Make this a generic trait implementable across
-    // any command descriptor
-    /// Returns a reference to a 6 byte slice of the descriptor.
-    pub fn as_slice(&'_ self) -> &[u8] {
-        // SAFETY: A struct is the size of itself
-        let slice: &'_ [u8] = unsafe {
-            let ptr = self as *const ShortCommandDescriptor as *const u8;
-            std::slice::from_raw_parts(ptr, std::mem::size_of::<ShortCommandDescriptor>())
-        };
-        slice
-    }
+impl CommandDescriptor for X6CommandDescriptor {}
+
+/// As described in SPC-2 4.3.2 table 2, a typical CDB for 10 byte commands.
+#[repr(C, packed)]
+struct X10CommandDescriptor {
+    ///"The `OPERATION CODE` field contains the code value identifying the operation
+    /// being requested by the CDB. SAM-2 defines the general structure of the operation
+    /// code value. The `OPERATION CODE` field has a consistently defined meaning across
+    /// all commands. This standard specifies the operation code values used by the commands
+    /// defined herein."
+    ///
+    /// This field specifies what command is being issued by the host
+    /// to the drive.
+    pub operation_code: OpCode,
+    /// (if required) the lower 5 bits identify a function to be performed under the
+    /// more general command specified in the `OPERATION CODE` field
+    pub service_action: u8,
+    /// The use of this field varies from command to command.
+    pub logical_block_address: [u8; 4],
+    /// Depending on the opcode, this field is one of `TRANSFER LENGTH` (amount of
+    /// data to be transferred, usually in blocks),
+    /// `PARAMETER LIST LENGTH` (number of bytes sent from the Data-Out buffer),
+    /// or `ALLOCATION LENGTH` (The maximum number of bytes a client has allocated for returned
+    /// data).
+    ///
+    ///More info can be found in SCSI SPC2 4.3
+    pub misc_len: [u8; 2],
+    /// "The contents of the `CONTROL` field are defined in SAM-2. The `CONTROL` field
+    /// has a consistently defined meaning across all commands."
+    ///
+    /// As far as I can tell, this value is set to zero by most modern implementations.
+    pub control: u8,
 }
+
+impl CommandDescriptor for X10CommandDescriptor {}
+
 /// "A command is communicated by sending a command descriptor block
 /// to the device ...."
 ///
@@ -171,7 +194,7 @@ impl ShortCommandDescriptor {
 /// "SCSI Primary Commands - 2 (SPC-2)" 4.3.2 The fixed length CDB formats
 /// Table 4 -- Typical CDB for 16-byte commands
 #[repr(C, packed)]
-struct LongCommandDescriptor {
+struct X16CommandDescriptor {
     ///"The `OPERATION CODE` field contains the code value identifying the operation
     /// being requested by the CDB. SAM-2 defines the general structure of the operation
     /// code value. The `OPERATION CODE` field has a consistently defined meaning across
@@ -241,13 +264,14 @@ struct LongCommandDescriptor {
     pub control: u8,
 }
 
-impl LongCommandDescriptor {
-    #[expect(unused)]
-    pub fn as_slice(&'_ self) -> &[u8] {
+impl CommandDescriptor for X16CommandDescriptor {}
+
+pub trait CommandDescriptor: Sized {
+    fn as_slice(&'_ self) -> &[u8] {
         // SAFETY: A struct is the size of itself
         let slice: &'_ [u8] = unsafe {
-            let ptr = self as *const LongCommandDescriptor as *const u8;
-            std::slice::from_raw_parts(ptr, std::mem::size_of::<LongCommandDescriptor>())
+            let ptr = self as *const Self as *const u8;
+            std::slice::from_raw_parts(ptr, std::mem::size_of::<Self>())
         };
         slice
     }
